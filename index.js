a826d1a98a1fe19de7a3d6cdbbf843b139f2c320
Maya S Rao <67920373+MayaSatishRao@users.noreply.github.com>
2022-04-08 16:28:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// getting all the HTML elements required for manipulation
+
 const btns = document.querySelectorAll(".buttons button");
 const guess = document.querySelector(".win");
 const img = document.querySelector(".image");
@@ -8,6 +10,12 @@ const grey = document.querySelector(".grey");
 const sound = new Audio("dices.mp3");
 const startButton = document.querySelector(".start-button button");
 
+
+// declaring all the constants required for functions.
+let i=5,ti=Math.floor(Math.random()*6)+1,score=0;
+let once=1;
+
+// adding event listener to start button and removing it
 startButton.addEventListener("click",()=>{
     startGame();
     diceThrow();
@@ -15,9 +23,39 @@ startButton.addEventListener("click",()=>{
     startButton.remove();
 });
 
-let i=5,ti=Math.floor(Math.random()*6)+1,score=0;
-let once=1;
+// startGame function which starts the game. 
+function startGame(){
+        time.style.display="inline";
+        // using template literals for changing the seconds
+        time.innerText=`The dice will change in ${i} seconds`;
+        // using setInterval method to call "increment" function every second
+        const b = setInterval(increment,1000);
+        diceThrow();
+    }
 
+// diceThrow function which picks a random dice face
+function diceThrow(){
+       // using template literals to change the image
+        img.style.backgroundImage = `url("images/dice${ti}.png")`;
+        ti = Math.floor(Math.random()*6)+1;
+}
+
+// increment function
+function increment(){
+    if(i==0){
+        i=5;
+        diceThrow();
+        sound.play();
+        once=1;
+    }
+    else
+    i--;
+    time.innerText = `The dice will change in ${i} seconds`;
+    grey.style.backgroundColor="#393E46";
+    guess.style.color="aliceblue";
+}
+
+// adding event listeners to all the buttons and check if guess is right or wrong
 btns.forEach(btn=>{
     btn.addEventListener("click",()=>{
        var clik = btn.getAttribute("value");
@@ -29,7 +67,8 @@ btns.forEach(btn=>{
            grey.style.backgroundColor="aliceblue";
            guess.style.color="#393E46";
            once++;
-           console.log(once);
+       }else if(clik==ti && once>1){
+          guess.innerText="Your guess was right";
        }else{
         guess.innerText="Your guess was wrong"; 
        }
@@ -39,34 +78,9 @@ btns.forEach(btn=>{
 
 
 
-function startGame(){
-    time.style.display="inline";
-        time.innerText=`The dice will change in ${i} seconds`;
-        const b = setInterval(increment,1000);
-        diceThrow();
-    }
 
 
-function diceThrow(){
-     
-     img.style.backgroundImage = `url("images/dice${ti}.png")`;
-     ti = Math.floor(Math.random()*6)+1;
-}
 
-function increment(){
-    if(i==0){
-        i=5;
-        diceThrow();
-        sound.play();
-        once=1;
-    }
-    else
-    i--;
-    time.innerText = `The dice will change in ${i} seconds`;
-    //console.log(once);
-    grey.style.backgroundColor="#393E46";
-    guess.style.color="aliceblue";
-}
 
 
     
